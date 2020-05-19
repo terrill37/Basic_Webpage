4bfc62ebea95420c4890a7e40582be66d4cc7c7a
Wesley Thomas Terrill <wterrill@lxplus602.cern.ch>
2020-05-19 19:11:32 +0200

    update

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<!-- visit w3schools.com/html/ for html tutorial-->
+
+<script src="https://kit.fontawesome.com/<access code>" crossorigin="anonymous"></script>
+<!-- the line above allows usage of icons
+     visit https://kit.fontawesome.com and create an account to get the access link to basic icons
+     use access value they assign you upon signing up 
+     not required for code to work-->
+
+<style>
+.button{
+    background-color: #4CAF50; //Green
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    }
+
+.button2{background-color: Crimson;}
+
+.navbar{                               
+    overflow: hidden;
+    background-color: #333;
+    font-family: Arial;
+    }
+
+.navbar-right{
+    float: right;
+    }
+
+.navbar a{
+    float: left;
+    font-size: 16px;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    }                               
+
+.dropdown{
+    float: left;
+    overflow: hidden;
+    }
+
+.dropdown .dropbtn{
+    font-size: 16px;
+    border: none;
+    outline: none;
+    color: white;
+    padding: 14px 16px;
+    background-color: inherit;
+    font-family: inherit;
+    margin: 0;
+    }
+
+.navbar a:hover{
+    background-color: red;
+    }
+
+.navbar a.active{
+    background-color: #4CAF50;
+    color: white;
+    }
+
+
+
+.dropdown:hover .dropbtn{
+    background-color: red;
+    }
+
+.dropdown-content{
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+    }
+
+.dropdown-content a{
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+    }
+
+.dropdown-content a:hover{
+    background-color: #ddd
+    }
+
+.dropdown:hover .dropdown-content{
+    display: block;
+}                                                                                   
+
+
+</style>
+
+<div class="navbar">
+    <div class="navbar-right">
+        <a class="active" href="/<User_Name/">
+            Home
+            <i class="fas fa-home"></i>
+        </a>
+    </div>
+    <a href="https://www.cmu.edu/physics/">
+        CMU Physics
+        <i class="fas fa-atom"></i>
+    </a>  
+    <div class="dropdown">
+        <button onclick="window.location.href='projects/';" class="dropbtn">Projects
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="#link1">link1</a>
+            <a href="#link2">link2</a>
+        </div>
+    </div>
+</div>                                                                                         
+
+<h1>User Name</h1>
+<h2>Homepage</h2>
+
+<img src="Profile_pic.jpeg" alt="Profile Picture" style="width:304px;height:304px;">
+<!-- in root directory include image file named as 'Profile_pic.jpeg' for image to show up-->
+
+</br>
+
+<?php
+    //visit w3schools.com/php/ for php tutorial 
+    echo "Hello World! <br>";
+?>
+
+</body>
+</html>
+
